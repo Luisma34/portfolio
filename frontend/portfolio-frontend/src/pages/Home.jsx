0b70dd7d1568cec5985e0b83f823b7901a9cb9aa
Luisma34 <luisma.cordon@gmail.com>
2026-04-17 17:19:45 +0100

    commit de seguridad

--- frontend/portfolio-frontend/src/pages/Home.jsx
+++ frontend/portfolio-frontend/src/pages/Home.jsx
@@ -1,11 +1,8 @@
 function Home() {
   return (
-    <div className="bg-black min-h-screen">
-      Home;
-      <p>Sección 1</p>
-      <div className="h-screen">espaciador</div>
-      <p>Sección 2</p>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
 
