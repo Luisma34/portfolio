35f8b4ddb036adbee5f4b0028c16d3a014f152da
Luisma34 <luisma.cordon@gmail.com>
2026-05-21 17:57:15 +0100

    archivos no añadidos

---
+++ frontend/portfolio-frontend/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+// Creamos el layout para no tener que modificar el tamaño de todos los componentes uno a uno.
+// {} Es para que podamos usar directamente lo que necesitemos.
+function Layout({ children }) {
+  return (
+    <div className="flex flex-col justify-center items-center md:max-w-6xl xl:max-w-full xl:px-15 xl:mx-8 md:mx-auto">
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
+
