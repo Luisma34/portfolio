846340fefd21b976e4aec043b6d048e4a9f77597
Luisma34 <luisma.cordon@gmail.com>
2026-05-24 21:15:52 +0100

    Carousel de stack

--- frontend/portfolio-frontend/src/components/TechStack.jsx
+++ frontend/portfolio-frontend/src/components/TechStack.jsx
@@ -1,7 +1,82 @@
 import Layout from "./Layout";
 
 function TechStack() {
-  return <Layout></Layout>;
+  const technologies = [
+    {
+      name: "Java",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
+    },
+    {
+      name: "React",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg",
+    },
+    {
+      name: "JavaScript",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg",
+    },
+    {
+      name: "TailwindCSS",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    },
+    {
+      name: "Spring",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg",
+    },
+    {
+      name: "HTML",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg",
+    },
+    {
+      name: "Bootstrap",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg",
+    },
+    {
+      name: "CSS",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain-wordmark.svg",
+    },
+    {
+      name: "GitHub",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg",
+    },
+    {
+      name: "Git",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-plain-wordmark.svg",
+    },
+    {
+      name: "MariaDB",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mariadb/mariadb-original-wordmark.svg",
+    },
+    {
+      name: "MySQL",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+    },
+  ];
+
+  const technologiesDoble = [...technologies, ...technologies];
+
+  return (
+    <>
+      <div className="py-52">
+        <Layout>
+          <h1 className="pb-5 ">Stack</h1>
+          <div className="carousel rounded-box w-full gap-5 flex items-center justify-center">
+            {technologiesDoble.map((tech, index) => (
+              // Entramos en el array buscando el indice de cada elemento para no duplicar nada.
+              // Hemos creado cada imagen de manera que simplificamos código.
+              <div key={index} className="carousel-item">
+                <img
+                  src={tech.src}
+                  alt={tech.name}
+                  className="w-25 object-contain"
+                />
+              </div>
+            ))}
+            ;
+          </div>
+        </Layout>
+      </div>
+    </>
+  );
 }
 
 export default TechStack;
