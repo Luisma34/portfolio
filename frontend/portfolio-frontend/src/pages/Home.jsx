6fb8acbedd889be061e781dc8f576fa33a4805bf
Luisma34 <luisma.cordon@gmail.com>
2026-06-24 18:18:17 +0100

    scroll 0,0

--- frontend/portfolio-frontend/src/pages/Home.jsx
+++ frontend/portfolio-frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Hero from "../components/Hero";
 import TechStack from "../components/TechStack";
 import Projects from "../components/Projects";
-
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Hero />
