19ad76d0c1d1e4a4703e3dae47d0f5f3582ee489
Luisma34 <luisma.cordon@gmail.com>
2026-08-01 18:45:31 +0100

    solo proyectos importantes

--- frontend/portfolio-frontend/src/components/Projects.jsx
+++ frontend/portfolio-frontend/src/components/Projects.jsx
@@ -3,8 +3,6 @@ import { motion } from "framer-motion";
 import ProjectsCard from "./ProjectsCard";
 import Layout from "./Layout";
 import inplayImg from "../assets/InplayEvents/foto-31.jpg";
-import libraryImg from "../assets/library-manager.png";
-import ticTacToeImg from "../assets/tic-tac-toe.png";
 
 function Projects({
   className,
@@ -18,7 +16,7 @@ function Projects({
         <Layout>
           <h2 className={headingClass}>{namePage}</h2>
           {/* IMPLEMENTAR LOCATION PARA CAMBIAR LAYOUT EN LA URL */}
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-2.5 md:gap-10 w-full">
+          <div className="grid grid-cols-1 gap-2.5 md:gap-10 w-full">
             {/* Inplay */}
             <motion.div
               initial={{ y: 50, opacity: 0 }}
@@ -34,40 +32,6 @@ function Projects({
                 className="object-top xl:object-top"
                 projectsUrl="https://github.com/Luisma34/Inplay-Events"
                 projectsDemo="https://luisma34.github.io/Inplay-Events/"
-              />
-            </motion.div>
-
-            {/* Library Manager */}
-            <motion.div
-              initial={{ y: 50, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.8, delay: 0.1 }}
-              viewport={{ once: true }}
-            >
-              <ProjectsCard
-                image={libraryImg}
-                project={"Library Manager"}
-                description={"Librería para administrar tu biblioteca personal."}
-                stack={"HTML, CSS, JavaScript Vanilla."}
-                projectsUrl="https://github.com/Luisma34/LibraryManager"
-                projectsDemo="https://luisma34.github.io/LibraryManager/"
-              />
-            </motion.div>
-
-            {/* Tic tac toe */}
-            <motion.div
-              initial={{ y: 50, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.8, delay: 0.2 }}
-              viewport={{ once: true }}
-            >
-              <ProjectsCard
-                image={ticTacToeImg}
-                project={"Tic Tac Toe"}
-                description={"Tres en raya para web Odin."}
-                stack={"HTML, CSS, JavaScript Vanilla."}
-                projectsUrl="https://github.com/Luisma34/Tres-en-raya-Tic-tac-toe"
-                projectsDemo="https://luisma34.github.io/Tres-en-raya-Tic-tac-toe/"
               />
             </motion.div>
           </div>
